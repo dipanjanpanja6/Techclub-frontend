530491952c6df48a01e2b6925a9859d8f58bfdf8
Akash Kumar Sen <71623442+Akash-Kumar-Sen@users.noreply.github.com>
2021-09-26 00:12:15 +0530

    Updates
    Latest update

--- src/pages/Home/Team.jsx
+++ src/pages/Home/Team.jsx
@@ -3,15 +3,18 @@ import ActionAreaCard from "./membercard"
 import { Grid, Typography, Fab } from "@material-ui/core"
 //store all member data in member.json file and use map to render it using ActionAreaCard component
 export default function Team() {
+  const name = "Ayush Jha"
+  const description = "President, Tech Club-GCECT"
+  const imageSource = "./team-members/ayushjha.jpeg"
+  const linkedIn = "https://www.linkedin.com/in/00-ayush-jha/"
+
   return (
     <div>
       <Typography style={{ padding: "10px 20px", textAlign: "center" }} variant="h5">
         Meet the Team
       </Typography>
-      <Grid container spacing={3} style={{ margin: "3% 5%" }}>
-        <ActionAreaCard />
-        <ActionAreaCard />
-        <ActionAreaCard />
+      <Grid container spacing={3} style={{ margin: "3% 1.5% " }}>
+        <ActionAreaCard name={name} description={description} imgSource={imageSource} link={linkedIn} />
       </Grid>
     </div>
   )
